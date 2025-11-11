e3eac7937172a0e71e5745bcb5c2ea55c525c144
Ilya Chistyakov <ichistyakovv@gmail.com>
2025-11-11 21:49:02 +0500

    refactor: refine intro message

--- internal/i18n/messages/en/intro.go
+++ internal/i18n/messages/en/intro.go
@@ -1,5 +1,5 @@
 package en
 
 var Intro = []string{
-	"This bot helps developers to intercept critical errors from your application in production and show them in convient way.",
+	"This bot helps developers to intercept critical errors from your application in production and show the errors in convient way.",
 }
