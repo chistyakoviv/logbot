73af7de18e51e7519aea1691af9834dba462b237
Ilya Chistyakov <ichistyakovv@gmail.com>
2025-11-16 20:39:16 +0500

    refactor: make i18n chain struct private

--- internal/i18n/chain.go
+++ internal/i18n/chain.go
@@ -5,28 +5,28 @@ import (
 	"fmt"
 )
 
-type I18nChain struct {
+type i18nChain struct {
 	i18n II18n
 	buf  bytes.Buffer
 }
 
 func NewI18nChain(i18n II18n) II18nChain {
-	return &I18nChain{
+	return &i18nChain{
 		i18n: i18n,
 		buf:  bytes.Buffer{},
 	}
 }
 
-func (c *I18nChain) T(lang string, key string, opts ...Option) II18nChain {
+func (c *i18nChain) T(lang string, key string, opts ...Option) II18nChain {
 	fmt.Fprintf(&c.buf, "%s", c.i18n.T(lang, key, opts...))
 	return c
 }
 
-func (c *I18nChain) String() string {
+func (c *i18nChain) String() string {
 	return c.buf.String()
 }
 
-func (c *I18nChain) Append(s string) II18nChain {
+func (c *i18nChain) Append(s string) II18nChain {
 	c.buf.WriteString(s)
 	return c
 }
