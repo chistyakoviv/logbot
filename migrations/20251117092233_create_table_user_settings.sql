e735a7f0cb69b3b101a7d107e45aecc5ff0cf6eb
Ilya Chistyakov <ichistyakovv@gmail.com>
2025-11-18 19:52:19 +0500

    chore: describe user settings db fields

--- migrations/20251117092233_create_table_user_settings.sql
+++ migrations/20251117092233_create_table_user_settings.sql
@@ -6,6 +6,12 @@ CREATE TABLE IF NOT EXISTS user_settings (
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
     PRIMARY KEY (user_id)
 )
+
+COMMENT ON TABLE user_settings IS 'User settings';
+
+COMMENT ON COLUMN user_settings.user_id IS 'User ID';
+COMMENT ON COLUMN user_settings.lang IS 'Current language of the user';
+COMMENT ON COLUMN user_settings.updated_at IS 'Last update timestamp';
 -- +goose StatementEnd
 
 -- +goose Down
