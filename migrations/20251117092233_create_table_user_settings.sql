0d0788d75628c24aaa32e09fbec064cbb280e5a3
Ilya Chistyakov <ichistyakovv@gmail.com>
2025-11-25 15:31:59 +0500

    fix: add missing semicolon

--- migrations/20251117092233_create_table_user_settings.sql
+++ migrations/20251117092233_create_table_user_settings.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS user_settings (
     lang INTEGER DEFAULT 0,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
     PRIMARY KEY (user_id)
-)
+);
 
 COMMENT ON TABLE user_settings IS 'User settings';
 
